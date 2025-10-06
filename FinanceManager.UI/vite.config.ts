5d1e4c94e548ed453a5b0bfdab29537960e92ae0
jscd <jisreserv@gmail.com>
2025-10-06 23:53:04 +0300

    Added docker compose for UI. Migrations now run from ts files

--- FinanceManager.UI/vite.config.ts
+++ FinanceManager.UI/vite.config.ts
@@ -5,6 +5,13 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   define: {
     // Define environment variables for client-side access
     'process.env': process.env,
